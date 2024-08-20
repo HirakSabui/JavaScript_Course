0446b59c07091aacd3733a1fd3ca9b888b66de72
HIRAK SABUI <138322702+HirakSabui@users.noreply.github.com>
2024-08-20 20:24:03 +0000

    varibles config

---
+++ .devcontainer/01_Basics/01_Variables.js
@@ -0,0 +1,15 @@
+const accountId = 23019009506638
+let accountEmail = "hiraksabui@gmail.com"
+var accountPassword ="Hir@k2003"
+accountCity = "Kolkata"
+
+// accountId = 55 // not allowed
+
+accountEmail = "ece2022029@rcciit.org.in"
+accountPassword = "LOKEN@TH"
+accountCity = "Chandannagore"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
