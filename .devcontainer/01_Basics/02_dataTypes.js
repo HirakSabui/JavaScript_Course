1fdb527de69db0a188fe936b5779c2cb960272d7
HIRAK SABUI <138322702+HirakSabui@users.noreply.github.com>
2024-08-21 20:01:31 +0000

    datatypes config

---
+++ .devcontainer/01_Basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all js code as a newer version
+
+// alert() //we are using nodejs, not browser
+
+// number = 2 to 55
+// bigint
+// string = ""
+// boolean = true/false
+// null = standalone value
+// undefined = 
+// symbol = unique
+
+// object
+
+console.log(typeof "Hirak");
