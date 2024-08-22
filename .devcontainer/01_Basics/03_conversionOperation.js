3f88f148661f4bb9fb44b42a581e8a26fa0fb1eb
HIRAK SABUI <138322702+HirakSabui@users.noreply.github.com>
2024-08-22 20:46:54 +0000

    conversionOpeation config

---
+++ .devcontainer/01_Basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+// ***************** Operations *************** //
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(+"")
+
+let num1, num2, num3
+num1=num2=num3=2+2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+let x = 3;
+const y = x++;
+console.log(`x:${x}, y:${y}`);
+let a = 3;
+const b = ++a;
+console.log(`a:${a}, b:${b}`);
